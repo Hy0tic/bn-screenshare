de22b761a8c6d73d74c68156842fb7c986b8141d
Hy0tic <vothanhdat278@gmail.com>
2023-07-31 00:19:32 -0400

    fix build issue

--- bnAPI/Program.cs
+++ bnAPI/Program.cs
@@ -8,7 +8,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllHeaders",
+        b =>
+        {
+            b.WithOrigins("http://127.0.0.1:5173", "https://thankful-bush-0e0949c0f.3.azurestaticapps.net")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,17 +29,6 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAllHeaders",
-        b =>
-        {
-            b.WithOrigins("https://thankful-bush-0e0949c0f.3.azurestaticapps.net/")
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials();
-        });
-});
 
 app.UseCors("AllowAllHeaders");
 app.MapControllers();
