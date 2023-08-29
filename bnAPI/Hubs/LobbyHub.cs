019983430fdb6d549bb91c261c8a109948c93f70
Hy0tic <vothanhdat278@gmail.com>
2023-08-29 02:10:26 -0400

    add signalR hub method for sending offer to lobby

--- bnAPI/Hubs/LobbyHub.cs
+++ bnAPI/Hubs/LobbyHub.cs
@@ -16,9 +16,9 @@ public class LobbyHub : Hub
     public async Task JoinLobby(string lobbyId)
     {
         var lobbyName = $"Lobby-{lobbyId}";
-        await Clients.Caller.SendAsync("JoinedGroup", lobbyId);
         await Clients.Group(lobbyName).SendAsync("MemberJoined", Context.ConnectionId);
         await Groups.AddToGroupAsync(Context.ConnectionId, lobbyName);
+        await Clients.Caller.SendAsync("JoinedGroup", lobbyId);
     }
 
     public async Task LeaveLobby(string lobbyId)
@@ -27,23 +27,22 @@ public class LobbyHub : Hub
         await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
         await Clients.Group(groupName).SendAsync("CallerLeft", $"{Context.ConnectionId} has left the group {groupName}.");
     }
-    
-    public async Task StartScreenSharing(ScreenSharingData data)
-    {
-        var frame = data.Frame;  // this is the frame received from the client
-        var lobbyId = data.LobbyId;  // this is the lobby id received from the client
-        Console.WriteLine(data.Frame);
-
-        var groupName = $"Lobby-{lobbyId}";
-
-        // now send the frame to all clients in the specified lobby
-        await Clients.Group(groupName).SendAsync("ReceiveScreenShare", frame);
-    }
-    
 
     public async Task SendOffer(string text, string uid)
     {
         await Clients.Client(uid).SendAsync("ReceivingOffer", text, Context.ConnectionId);
+    }
+
+    public async Task SendOfferToLobby(string lobbyId, string text)
+    {
+        var lobbyName = $"Lobby-{lobbyId}";
+        await Clients.OthersInGroup(lobbyName).SendAsync("ReceivingOffer", text,Context.ConnectionId);
+    }
+
+    public async Task SendMessage(string message, string lobbyId)
+    {
+        var groupName = $"Lobby-{lobbyId}";
+        await Clients.Group(groupName).SendAsync("ReceiveMessage", message);
     }
     
 }
