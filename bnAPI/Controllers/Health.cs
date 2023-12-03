803b85287bfa109d601fd21fdf23b79f798f2b9f
Hy0tic <vothanhdat278@gmail.com>
2023-12-03 03:00:54 -0500

    move build info endpoint into its own controller

---
+++ bnAPI/Controllers/Health.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using BnScreenshareAPI;
+
+namespace bnAPI.Controllers;
+
+public class Health : Controller
+{
+    
+    [HttpGet("/getVersion")]
+    public IActionResult Version()
+    {
+        const string filePath = "buildinfo.json";
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound();
+        }
+        
+        var jsonString = System.IO.File.ReadAllText(filePath);
+        var jsonData = JsonSerializer.Deserialize<Dictionary<string, BuildInfo>>(jsonString);
+
+        var buildInfo = jsonData["buildInfo"];
+
+        return Ok(buildInfo);
+    }
+}
--- bnAPI/Controllers/LobbyController.cs
+++ bnAPI/Controllers/LobbyController.cs
@@ -12,22 +12,5 @@ public class LobbyController : Controller
     {
         return Ok("Hello This Is BnScreenshareAPI");
     }
-
-    [HttpGet("/getVersion")]
-    public IActionResult Version()
-    {
-        const string filePath = "buildinfo.json";
-        if (!System.IO.File.Exists(filePath))
-        {
-            return NotFound();
-        }
-        
-        string jsonString = System.IO.File.ReadAllText(filePath);
-        var jsonData = JsonSerializer.Deserialize<Dictionary<string, BuildInfo>>(jsonString);
-
-        var buildInfo = jsonData["buildInfo"];
-
-        return Ok(buildInfo);
-    }
-
+    
 }
